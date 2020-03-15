9d729b65bbc6c2b2e4264c85a1625c269f0734df
trexr <6519611+trexr@users.noreply.github.com>
2020-03-15 12:36:46 -0700

    Create bash cat script

--- build.sh
+++ build.sh
@@ -1,1 +1,16 @@
-cat templates/top.html about.html templates/bottom.html > docs/about.html
+
+target="*.html"
+# if no arguments then build all html files
+for file in ${target}
+do
+  filename=${file%%.*}
+  cat templates/top.html ${filename}.html templates/bottom.html > docs/${filename}.html
+  echo "${filename}.html created"
+
+  sed -i "" "s/{{FILE_NAME}}/${filename}/g" docs/${filename}.html
+  
+  # Other possibility
+  # grep -l 'FILE_NAME' docs/${filename}.html | xargs sed -i 's/FILE_NAME/test/g'
+  
+  open docs/${filename}.html
+done
