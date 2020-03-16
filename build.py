be60b6e8cdee834c5197b3f6ad777252d7416b91
trexr <6519611+trexr@users.noreply.github.com>
2020-03-15 23:48:50 -0700

    Changes to use Template Lib

--- build.py
+++ build.py
@@ -1,4 +1,5 @@
 
+from string import Template
 import glob
 
 targetfiles = "*.html"
@@ -6,14 +7,30 @@ TMPL_DIR = "templates/"
 TMPL_TOP = TMPL_DIR + "top.html"
 TMPL_BOTTOM = TMPL_DIR + "bottom.html"
 
+# for fname in glob.glob(targetfiles):
+#     name = fname.replace('.html', '')
+#     name = 'home' if name == 'index' else name
+#     top = open(TMPL_TOP).read()
+#     top = top.replace(name+"-link", name + "-link active")
+#     bottom = open(TMPL_BOTTOM).read()
+#     fnamecontent = open(fname).read()
+#     catcontent = "{} {} {}\n".format(top, fnamecontent, bottom)
+#     catcontent = catcontent.replace('{{FILE_NAME}}', name)
+#     open('docs/'+fname, 'w+').write(catcontent)
+#     print("Created in docs:", fname)
+
+
+template_text = open('page_template.html').read()
+template = Template(template_text)
+
+
+# Use "format" feature of Python strings to insert data where needed for the
+# index page
 for fname in glob.glob(targetfiles):
-    name = fname.replace('.html', '')
-    name = 'home' if name == 'index' else name
-    top = open(TMPL_TOP).read()
-    top = top.replace(name+"-link", name + "-link active")
-    bottom = open(TMPL_BOTTOM).read()
-    fnamecontent = open(fname).read()
-    catcontent = "{} {} {}\n".format(top, fnamecontent, bottom)
-    catcontent = catcontent.replace('{{FILE_NAME}}', name)
-    open('docs/'+fname, 'w+').write(catcontent)
-    print("Created in docs:", fname)
+    middle_content = open(fname).read()
+    page_html = template.safe_substitute(
+        title=fname.replace('.html', ''),
+        index_class="active",
+        content=middle_content,
+    )
+    open('docs/' + fname, 'w+').write(page_html)
