a32af949d2e4986d28c13fe5fe83a337df86d7c1
trexr <6519611+trexr@users.noreply.github.com>
2020-03-20 14:31:27 -0700

    Cleans and adds nav template to base

--- build.py
+++ build.py
@@ -1,6 +1,5 @@
 
 from string import Template
-import glob
 
 
 def mock_data():
@@ -25,43 +24,34 @@ def main():
     TMPL_DIR = "templates/"
     TMPL_TOP = TMPL_DIR + "top.html"
     TMPL_BOTTOM = TMPL_DIR + "bottom.html"
+    TMPL_BASE = TMPL_DIR + "base.html"
     BUILD_DIR = "docs/"
-
     PAGE_DATA = mock_data()
+    TMPL_NAV = TMPL_DIR + "nav.html"
 
     # create tempalte to reference in iteration over page
-    template_text = open(TMPL_DIR+'page_template.html').read()
+    template_text = open(TMPL_BASE).read()
     template = Template(template_text)
 
-    # iterate over each page invoke template with keys from page
+    # iterate over each page invoke template with datum
     for page in PAGE_DATA:
-        title = page.get("title")
+        title = page["title"]
+        output = page["output"]
+        content = page["filename"]
 
-        if page.get("filename"):
+        if output and len(output):
             page_html = template.safe_substitute(
                 title=title,
+                nav=open(TMPL_NAV).read(),
                 index_class="active",
-                output=page.get("output") or BUILD_DIR +
-                title + ".html"
+                content=open(content).read()
             )
-            open(BUILD_DIR + fname, 'w+').write(page_html)
+            print(open(TMPL_NAV).read())
+            open(output, 'w+').write(page_html)
+            print("Created", title, "page")
 
         else:
-            print("missing page")
-    # for fname in glob.glob(targetfiles):
-    #     name = fname.replace('.html', '')
-    #     name = 'home' if name == 'index' else name
-    #     top = open(TMPL_TOP).read()
-    #     top = top.replace(name+"-link", name + "-link active")
-    #     bottom = open(TMPL_BOTTOM).read()
-    #     fnamecontent = open(fname).read()
-    #     catcontent = "{} {} {}\n".format(top, fnamecontent, bottom)
-    #     catcontent = catcontent.replace('{{FILE_NAME}}', name)
-    #     open('docs/'+fname, 'w+').write(catcontent)
-    #     print("Created in docs:", fname)
-
-    # Use "format" feature of Python strings to insert data where needed for the
-    # index page
+            print("Content file is missing")
 
 
 main()
