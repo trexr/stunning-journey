73ef3b6b0afb7cd9e36e1440c22ac35e92bab3ac
trexr <6519611+trexr@users.noreply.github.com>
2020-03-20 13:48:06 -0700

    Updates with mockdate and template generating script

--- build.py
+++ build.py
@@ -2,35 +2,66 @@
 from string import Template
 import glob
 
-targetfiles = "*.html"
-TMPL_DIR = "templates/"
-TMPL_TOP = TMPL_DIR + "top.html"
-TMPL_BOTTOM = TMPL_DIR + "bottom.html"
 
-# for fname in glob.glob(targetfiles):
-#     name = fname.replace('.html', '')
-#     name = 'home' if name == 'index' else name
-#     top = open(TMPL_TOP).read()
-#     top = top.replace(name+"-link", name + "-link active")
-#     bottom = open(TMPL_BOTTOM).read()
-#     fnamecontent = open(fname).read()
-#     catcontent = "{} {} {}\n".format(top, fnamecontent, bottom)
-#     catcontent = catcontent.replace('{{FILE_NAME}}', name)
-#     open('docs/'+fname, 'w+').write(catcontent)
-#     print("Created in docs:", fname)
-
-
-template_text = open('page_template.html').read()
-template = Template(template_text)
+def mock_data():
+    data = [
+        {"filename": "content/about.html",
+            "output": "docs/about.html",
+            "title": "about"
+         },
+        {"filename": "content/index.html",
+            "output": "docs/index.html",
+            "title": "index"
+         },
+        {"filename": "content/work.html",
+            "output": "docs/work.html",
+            "title": "work"}
+    ]
+    return data
 
 
-# Use "format" feature of Python strings to insert data where needed for the
-# index page
-for fname in glob.glob(targetfiles):
-    middle_content = open(fname).read()
-    page_html = template.safe_substitute(
-        title=fname.replace('.html', ''),
-        index_class="active",
-        content=middle_content,
-    )
-    open('docs/' + fname, 'w+').write(page_html)
+def main():
+    targetfiles = "*.html"
+    TMPL_DIR = "templates/"
+    TMPL_TOP = TMPL_DIR + "top.html"
+    TMPL_BOTTOM = TMPL_DIR + "bottom.html"
+    BUILD_DIR = "docs/"
+
+    PAGE_DATA = mock_data()
+
+    # create tempalte to reference in iteration over page
+    template_text = open(TMPL_DIR+'page_template.html').read()
+    template = Template(template_text)
+
+    # iterate over each page invoke template with keys from page
+    for page in PAGE_DATA:
+        title = page.get("title")
+
+        if page.get("filename"):
+            page_html = template.safe_substitute(
+                title=title,
+                index_class="active",
+                output=page.get("output") or BUILD_DIR +
+                title + ".html"
+            )
+            open(BUILD_DIR + fname, 'w+').write(page_html)
+
+        else:
+            print("missing page")
+    # for fname in glob.glob(targetfiles):
+    #     name = fname.replace('.html', '')
+    #     name = 'home' if name == 'index' else name
+    #     top = open(TMPL_TOP).read()
+    #     top = top.replace(name+"-link", name + "-link active")
+    #     bottom = open(TMPL_BOTTOM).read()
+    #     fnamecontent = open(fname).read()
+    #     catcontent = "{} {} {}\n".format(top, fnamecontent, bottom)
+    #     catcontent = catcontent.replace('{{FILE_NAME}}', name)
+    #     open('docs/'+fname, 'w+').write(catcontent)
+    #     print("Created in docs:", fname)
+
+    # Use "format" feature of Python strings to insert data where needed for the
+    # index page
+
+
+main()
