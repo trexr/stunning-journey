a1225de6d1a3a89e9522f9e36677eda2c0c5da7b
trexr <6519611+trexr@users.noreply.github.com>
2020-03-20 17:44:39 -0700

    Refactors

--- build.py
+++ build.py
@@ -1,7 +1,12 @@
-
 from string import Template
 
 
+def main():
+    *FIRST, PAGE_DATA = constants()
+    build_content(PAGE_DATA)
+
+
+# mock service package
 def mock_data():
     data = [
         {"filename": "content/about.html",
@@ -19,39 +24,51 @@ def mock_data():
     return data
 
 
-def main():
-    targetfiles = "*.html"
+def constants():
     TMPL_DIR = "templates/"
-    TMPL_TOP = TMPL_DIR + "top.html"
-    TMPL_BOTTOM = TMPL_DIR + "bottom.html"
-    TMPL_BASE = TMPL_DIR + "base.html"
     BUILD_DIR = "docs/"
     PAGE_DATA = mock_data()
+    TMPL_BASE = TMPL_DIR + "base.html"
     TMPL_NAV = TMPL_DIR + "nav.html"
+    return [TMPL_BASE, TMPL_DIR, TMPL_NAV, BUILD_DIR, PAGE_DATA]
 
-    # create tempalte to reference in iteration over page
-    template_text = open(TMPL_BASE).read()
+
+def read_file(path):
+    return open(path).read()
+
+
+def create_page(pathandfile, content):
+    if pathandfile and content:
+        open(pathandfile, 'w+').write(content)
+    else:
+        print("Missing path to file or content ")
+
+
+def swap_file_path_for_text(keyword, collection):
+    for key in collection:
+        if(key == keyword):
+            collection[key] = read_file(collection[key])
+    return collection
+
+
+def build_content(data):
+    TMPL_BASE, TMPL_DIR, TMPL_NAV, BUILD_DIR, PAGE_DATA = constants()
+
+    # generate template for page creation
+    template_text = read_file(TMPL_BASE)
     template = Template(template_text)
+    nav = read_file(TMPL_NAV)
 
-    # iterate over each page invoke template with datum
-    for page in PAGE_DATA:
-        title = page["title"]
-        output = page["output"]
-        content = page["filename"]
-
-        if output and len(output):
-            page_html = template.safe_substitute(
-                title=title,
-                nav=open(TMPL_NAV).read(),
-                index_class="active",
-                content=open(content).read()
-            )
-            print(open(TMPL_NAV).read())
-            open(output, 'w+').write(page_html)
-            print("Created", title, "page")
-
-        else:
-            print("Content file is missing")
+    for page in data:
+        # add in nav template
+        # will constomize this to inject appropriate active class in nav in future
+        if not "nav" in page:
+            page["nav"] = nav
+            print(page)
+        # create mappings for page generation
+        tmpl_mapping = swap_file_path_for_text("filename", page)
+        page_html = template.safe_substitute(tmpl_mapping)
+        create_page(tmpl_mapping["output"], page_html)
 
 
 main()
