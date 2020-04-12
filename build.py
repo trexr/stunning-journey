3bc6395dd1f6179f9f585014401d699766eb55aa
trexr <6519611+trexr@users.noreply.github.com>
2020-04-11 19:48:03 -0700

    Updates to jinja tmpl

--- build.py
+++ build.py
@@ -2,47 +2,47 @@
 import sys
 import glob
 import os
-from jinja2 import Template
+from jinja2 import Template, Environment, BaseLoader
 
 # TODO:
 # Need to templatize navigation and remove markup from base template,
 # Right now using function to create list items then inject that into
 #  base template instead of the entire nav
 
+TMPL_DIR = "templates/"
+BUILD_DIR = "docs/"
+TMPL_BASE = TMPL_DIR + "base.html"
+TMPL_NAV = TMPL_DIR + "nav.html"
+NAV_TITLES = {
+    "index": "Trevor Stearns"
+}
+
 
 def main():
-    TMPL_DIR = "templates/"
-    BUILD_DIR = "docs/"
-    PAGE_DATA = pages_to_build()
-    TMPL_BASE = TMPL_DIR + "base.html"
-    TMPL_NAV = TMPL_DIR + "nav.html"
 
-    template_text = open(TMPL_BASE).read()
-    # create template for the base HTML pages
-    template = Template(template_text)
+    PAGE_DATA = update_navtitles(pages_to_build())
+    nav_file = open(TMPL_NAV).read()
+    nav_tmpl = Template(nav_file)
+
+    template_html = open(TMPL_BASE).read()
+    template = Template(template_html)
 
     # Loops over each "page" dict in  return data list
 
     for page in PAGE_DATA:
+
         # Adding in nav key to each page dictionary if
         # its not already there
         # Doing this because we may want to add a different nav template
         # for certain pages in future
         if "pagenavigation" not in page:
 
-            file_nav = open(TMPL_NAV).read()
-            tmpl_nav = Template(file_nav)
-            nav_html = tmpl_nav.render(pages=PAGE_DATA, current=page['title'])
+            render_nav_html = nav_tmpl.render(
+                pages=PAGE_DATA, current=page['title'])
+
             # Passes current page into generated navigation html for
             # hightlighting its active state
-
-            # nav = create_navigation_HTML(PAGE_DATA, page['title'])
-            page["pagenavigation"] = nav_html
-
-        page["pages"] = PAGE_DATA`
-
-        template_html = open(TMPL_BASE).read()
-        template = Template(template_html)
+            page["pagenavigation"] = render_nav_html
 
         # mutates 'filename' value and assigns HTML found in "filename" key
         tmpl_mapping = swap_file_for_html("filename", page)
@@ -51,9 +51,16 @@ def main():
         page_html = template.render(tmpl_mapping)
 
         #  Use "output" value for filename and save to specified directory
-        create_file(tmpl_mapping["output"], page_html, BUILD_DIR)
+        create_file(page["output"], page_html, BUILD_DIR)
 
     print("Build complete")
+
+
+def update_navtitles(data):
+    for page in data:
+        if page['navtitle'] in NAV_TITLES:
+            page['navtitle'] = NAV_TITLES[page['navtitle']]
+    return data
 
 
 def pages_to_build():
@@ -64,12 +71,12 @@ def pages_to_build():
     for file_path in html_files:
         file_name = os.path.basename(file_path)
         name_only, extension = os.path.splitext(file_name)
-        print(name_only)
+
         pages.append(
             {"filename": file_path,
              "output": file_name,
              "title": name_only,
-             "nav-title": name_only
+             "navtitle": name_only
              })
 
     return pages
